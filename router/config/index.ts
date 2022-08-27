dd303543027e8ea29a65634b024f9ffb67609be2
saveliy <ssandry.github@gmail.com>
2022-08-27 21:49:17 +0300

    Add screens

--- router/config/index.ts
+++ router/config/index.ts
@@ -4,15 +4,37 @@ import { IRoute } from "./types";
 import { InitializeScreen } from "@screens/Initialize";
 import { MainScreen } from "@screens/Main";
 import { AboutScreen } from "@screens/About";
+import { LoadingScreen } from "@screens/Loading";
+import { TransactionsScreen } from "@screens/Transactions";
+import { AddGoalScreen } from "@screens/AddGoal";
+import { WelcomeInitialScreen } from "@screens/Welcome/Initial";
+import { WelcomeConceptScreen } from "@screens/Welcome/Concept";
+import { WelcomeMeetScreen } from "@screens/Welcome/Meet";
+import { WelcomeAddGoalScreen } from "@screens/Welcome/AddGoal";
 
-const INITIAL_ROUTE_NAME: string = "InitializeScreen";
-
+/** Applization routes */
 const routes: IRoute[] = [
+  /** Init screen */
   { name: "InitializeScreen", component: InitializeScreen },
+
+  /** Main screens */
   { name: "MainScreen", component: MainScreen },
-  { name: "About", component: AboutScreen },
+  { name: "LoadingScreen", component: LoadingScreen },
+  { name: "TransactionsScreen", component: TransactionsScreen },
+  { name: "AddGoalScreen", component: AddGoalScreen },
+  { name: "AboutScreen", component: AboutScreen },
+
+  /** Welcome screens */
+  { name: "WelcomeInitialScreen", component: WelcomeInitialScreen },
+  { name: "WelcomeConceptScreen", component: WelcomeConceptScreen },
+  { name: "WelcomeMeetScreen", component: WelcomeMeetScreen },
+  { name: "WelcomeAddGoalScreen", component: WelcomeAddGoalScreen },
 ];
 
+/** Application preloading screen name */
+const INITIAL_ROUTE_NAME: string = "InitializeScreen";
+
+/** Application preloading screen */
 const initialRoute: IRoute =
   routes.find(function findInitialScreen(route) {
     return route.name === INITIAL_ROUTE_NAME;
--- router/index.tsx
+++ router/index.tsx
@@ -7,23 +7,20 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 /** Router config */
 import { initialRoute, INITIAL_ROUTE_NAME, routes } from "./config";
 
-function Router() {
+function Router(): JSX.Element {
   const Stack = createNativeStackNavigator();
+
+  const screenOptions = {
+    headerShown: false,
+  };
 
   return (
     <Fragment>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName={INITIAL_ROUTE_NAME}>
+        <Stack.Navigator initialRouteName={INITIAL_ROUTE_NAME || initialRoute.name}>
           {routes.map(function initializeRoutes(route): JSX.Element {
             return (
-              <Stack.Screen
-                options={{
-                  headerShown: false,
-                }}
-                key={route.name}
-                name={route.name}
-                component={route.component}
-              />
+              <Stack.Screen options={screenOptions} key={route.name} name={route.name} component={route.component} />
             );
           })}
         </Stack.Navigator>
