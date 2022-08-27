98024dc17bea1d2f202b925edea1ea92e63ecd15
saveliy <ssandry.github@gmail.com>
2022-08-27 16:32:18 +0300

    Absolute paths

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,28 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./"],
+          extensions: [".ios.js", ".android.js", ".js", ".ts", ".tsx", ".json"],
+          alias: {
+            "@components": "./components",
+            "@screens": "./screens",
+            "@router": "./router",
+            "@assets": "./assets",
+            "@icons": "./icons",
+            "@hooks": "./hooks",
+            "@constants": "./constants",
+            "@store": "./store",
+            "@sql": "./sql",
+            "@layouts": "./layouts",
+            "@types": "./types",
+            "@localization": "./localization",
+          },
+        },
+      ],
+    ],
   };
 };
