b4690fb6b618e2ac368ce1286f5cf33a6ed44070
saveliy <ssandry.github@gmail.com>
2022-08-27 09:33:03 +0300

    Add more data

--- constants/meta.ts
+++ constants/meta.ts
@@ -1,6 +1,14 @@
 const PROJECT = {
   name: "Aventa",
   version: "0.01",
+  repository: "https://github.com/hschhhwwwo0o/aventa",
+  description: "Grow a garden from the accumulation, dont forget to water the flowers after the replenishments. ",
+  designers: [
+    {
+      tag: "@hschhhwwwo0o",
+      link: "https://www.behance.net/hschhhwwwo0o",
+    },
+  ],
 };
 
 Object.freeze(PROJECT);
