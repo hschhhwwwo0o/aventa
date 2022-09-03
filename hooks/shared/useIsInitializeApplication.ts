f027f0aa38c8cc445c92d279d0ad44c45f5d038e
saveliy <ssandry.github@gmail.com>
2022-09-03 12:38:41 +0300

    Fix is initialize hook

--- hooks/shared/useIsInitializeApplication.ts
+++ hooks/shared/useIsInitializeApplication.ts
@@ -1,11 +1,17 @@
 import { useEffect, useState } from "react";
 
+/** Utils */
+import { getIsInitializeFlagInStorage } from "@utils/initialize/getIsInitializeFlagInStorage";
+
 function useIsInitializeApplication(): boolean | "loading" {
   const [isInitialize, setIsInitialize] = useState<boolean | "loading">("loading");
 
-  useEffect(function detectIsInitializeFlag(): void {
-    setIsInitialize(false);
-  });
+  useEffect(function (): void {
+    (async function detectIsInitializeFlag(): Promise<void> {
+      const storedFlag: boolean = await getIsInitializeFlagInStorage();
+      setIsInitialize(storedFlag);
+    })();
+  }, []);
 
   return isInitialize;
 }
