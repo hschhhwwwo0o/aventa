b986d7b906bf04a66c85c934bc03b461d777811b
saveliy <ssandry.github@gmail.com>
2022-09-10 08:06:00 +0300

    Refactoring routes

--- router/config/types.d.ts
+++ router/config/types.d.ts
@@ -3,4 +3,11 @@ import { FunctionComponent } from "react";
 export interface IRoute {
   name: string;
   component: any;
+  options?: any;
+}
+
+/** @see https://reactnavigation.org/docs/group/ */
+export interface IRoutesGroup {
+  name: string;
+  routes: IRoute[];
 }
