ef8efba9749112949078832a58b995a9577aa0b9
saveliy <ssandry.github@gmail.com>
2022-09-14 05:42:30 +0300

    Fix

--- layouts/DefaultLayout/styles.ts
+++ layouts/DefaultLayout/styles.ts
@@ -1,7 +1,10 @@
 import { StyleSheet } from "react-native";
+import { HORIZONTAL_LAYOUT_PADDING, VERTICAL_LAYOUT_PADDING } from "~/styles/constants/sizes";
 
 export default StyleSheet.create({
   container: {
     flex: 1,
+    paddingHorizontal: HORIZONTAL_LAYOUT_PADDING,
+    paddingTop: VERTICAL_LAYOUT_PADDING / 2,
   },
 });
