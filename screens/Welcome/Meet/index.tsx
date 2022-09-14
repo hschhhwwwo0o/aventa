cd3ab4b6d430698783ec97c8ad380854f88c68fa
saveliy <ssandry.github@gmail.com>
2022-09-14 06:01:15 +0300

    Init screen

--- screens/Welcome/Meet/index.tsx
+++ screens/Welcome/Meet/index.tsx
@@ -2,11 +2,21 @@ import { Fragment } from "react";
 
 /** Layouts */
 import { BGLayout } from "~/layouts/BGLayout";
+import { TapForContinueLayout } from "~/layouts/TapForContinueLayout";
+
+/** Components */
+import { TheHeader } from "~/components/ui/TheHeader";
 
 function WelcomeMeetScreen({ navigation, route }: IScreen): JSX.Element {
   return (
     <Fragment>
-      <BGLayout type="A"></BGLayout>
+      <BGLayout type="A">
+        <TheHeader isBack />
+        <TapForContinueLayout
+          nextScreen="Welcome/AddGoal"
+          styleOptions={{ withTapHint: false, withTopPadding: true }}
+        ></TapForContinueLayout>
+      </BGLayout>
     </Fragment>
   );
 }
