8bb7753a8c64487d7d91d368ad989b195cca191e
saveliy <ssandry.github@gmail.com>
2022-09-23 05:35:55 +0300

    Add sqlite client interface

--- databases/sqlite/client.ts
+++ databases/sqlite/client.ts
@@ -1,10 +1,41 @@
 import { initializeSQLiteTables } from "./_/initializeTables.sqlite";
 import { SQLiteDatabase } from "./_/database.sqlite";
 
-export const SQLiteClient = Object.freeze({
+/** Queries */
+import { deleteGoalQuery } from "./_/goals/deleteGoal";
+import { insertGoalQuery } from "./_/goals/insertGoal";
+import { selectGoalQuery } from "./_/goals/selectGoal";
+import { selectGoalsQuery } from "./_/goals/selectGoals";
+import { insertTransactionQuery } from "./_/transactions/insertTransaction";
+import { selectTransactionsQuery } from "./_/transactions/selectTransactions";
+
+interface ITableQueries {
+  delete?: (any?: any) => void;
+  insert?: (any?: any) => void;
+  select?: (any?: any) => void;
+  selectMany?: (any?: any) => void;
+}
+
+export interface ISQLiteClient {
+  initialize: () => void;
+  table: {
+    goals: ITableQueries;
+    transactions: ITableQueries;
+  };
+}
+
+export const SQLiteClient: ISQLiteClient = Object.freeze({
   initialize: initializeSQLiteTables,
   table: {
-    goals: {},
-    transactions: {},
+    goals: {
+      delete: deleteGoalQuery,
+      insert: insertGoalQuery,
+      select: selectGoalQuery,
+      selectMany: selectGoalsQuery,
+    },
+    transactions: {
+      insert: insertTransactionQuery,
+      selectMany: selectTransactionsQuery,
+    },
   },
 });
